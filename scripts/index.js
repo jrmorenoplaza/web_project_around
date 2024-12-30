04e66e70611da9345a36bb2ab35e07ae3c2c3658
JesusRodrigo <jesusrodrigomp19@gmail.com>
2024-12-30 00:52:31 -0600

    Se hace la validacion de los formularios y se agregan clases para los errores correspondientes

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,67 @@
+/*import { enableValidation, resetFormValidation } from './validate.js';*/
+
+function showInputError(inputElement, config) {
+    const errorElement = document.querySelector(`#${inputElement.id}-error`);
+    if (!inputElement.validity.valid) {
+        errorElement.textContent = inputElement.validationMessage;
+        errorElement.classList.add(config.errorClass);
+        inputElement.classList.add(config.inputErrorClass);
+    } else {
+        errorElement.textContent = '';
+        errorElement.classList.remove(config.errorClass);
+        inputElement.classList.remove(config.inputErrorClass);
+    }
+
+    const formElement = inputElement.closest(config.formSelector);
+    toggleButtonState(formElement, config);
+}
+
+
+function toggleButtonState(formElement, config) {
+    const buttonElement = formElement.querySelector(config.submitButtonSelector);
+    const inputElements = Array.from(formElement.querySelectorAll(config.inputSelector));
+    const isFormValid = inputElements.every((input) => input.validity.valid && input.value.trim() !== '');
+    
+    if (isFormValid) {
+        buttonElement.disabled = false;
+        buttonElement.classList.remove(config.inactiveButtonClass);
+    } else {
+        buttonElement.disabled = true;
+        buttonElement.classList.add(config.inactiveButtonClass);
+    }
+}
+
+
+function setEventListeners(formElement, config) {
+    const inputElements = Array.from(formElement.querySelectorAll(config.inputSelector));
+    inputElements.forEach((inputElement) => {
+        inputElement.addEventListener('input', () => {
+            showInputError(inputElement, config);
+            toggleButtonState(formElement, config);
+        });
+    });
+}
+
+function enableValidation(config) {
+    const formElements = Array.from(document.querySelectorAll(config.formSelector));
+    formElements.forEach((formElement) => {
+        formElement.addEventListener('submit', (e) => e.preventDefault());
+        setEventListeners(formElement, config);
+        toggleButtonState(formElement, config);
+    });
+}
+
+const validationConfig = {
+    formSelector: '.popup__form, .add__form',
+    inputSelector: 'input',
+    submitButtonSelector: '.popup__save, .add__save',
+    inactiveButtonClass: 'popup__button_disabled',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__error_visible'
+};
+
+enableValidation(validationConfig);
+
 function disableDeleteButtons() {
     const deleteButtons = document.querySelectorAll('.card__delete');
     deleteButtons.forEach((button) => {
@@ -58,6 +122,9 @@ const openPopup = () => {
 
     toggleSaveButton();
     disableDeleteButtons();
+
+    const profileForm = document.querySelector('.popup__form');
+    resetFormValidation(profileForm, validationConfig);
 };
 
 const toggleSaveButton = () => {
@@ -92,9 +159,13 @@ const addFormOverlay = document.querySelector('.overlay');
 const openAddForm = () => {
     addPopup.classList.remove('hidden');
     addFormOverlay.classList.remove('hidden');
-    toggleAddSaveButton(); 
+    toggleAddSaveButton();
     disableDeleteButtons();
+
+    const addForm = document.querySelector('.add__form');
+    resetFormValidation(addForm, validationConfig);
 };
+
 
 const closeAddForm = () => {
     addPopup.classList.add('hidden');
@@ -118,8 +189,8 @@ addButton.addEventListener('click', openAddForm);
 addCloseButton.addEventListener('click', closeAddForm);
 addFormOverlay.addEventListener('click', closeAddForm);
 
-document.querySelector('.add__name').addEventListener('input', toggleAddSaveButton);
-document.querySelector('.add__url').addEventListener('input', toggleAddSaveButton);
+document.querySelector('#place-name').addEventListener('input', toggleAddSaveButton);
+document.querySelector('#place-url').addEventListener('input', toggleAddSaveButton);
 
 document.addEventListener('keydown', (e) => {
     if (e.key === 'Escape' && !addPopup.classList.contains('hidden')) {
@@ -246,6 +317,7 @@ document.addEventListener('keydown', (e) => {
         closeZoomPopup();
     }
 });
+
 
 
 
